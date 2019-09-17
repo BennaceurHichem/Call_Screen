f74ae37e134923b5e1f90ca39d91cbb01fa3a0a4
BennaceurHichem <gh_bennaceur@esi.dz>
2019-09-17 21:45:29 +0100

    image rounded

--- app/src/main/java/com/example/yassirfirstessay/MainActivity.java
+++ app/src/main/java/com/example/yassirfirstessay/MainActivity.java
@@ -1,26 +1,43 @@
 package com.example.yassirfirstessay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.MotionEventCompat;
+import androidx.core.view.ScaleGestureDetectorCompat;
 
+import android.animation.ObjectAnimator;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.makeramen.roundedimageview.RoundedImageView;
+import com.mikhaellopez.circularimageview.CircularImageView;
 import com.skyfishjy.library.RippleBackground;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
 
        private  TextView src,srcChemin,dest,destChemin,costMessage,costValue,time ;
         private ImageButton destCircle,srcCircle;
         private ImageView fleche;
         private View view1,view2;
-        RoundedImageView riv;
+         private boolean stayShaked;
+        private RippleBackground rippleBack;
+        private ViewGroup mRrootLayout;
+        private int _xDelta;
+        private int _yDelta;
+       private ImageView img;
+    private GestureDetector mGestureDetector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         costMessage = findViewById(R.id.cost_message);
         costValue = findViewById(R.id.cost_value);
         time = findViewById(R.id.time);
+        img= findViewById(R.id.user_img);
 
         srcChemin.bringToFront();
         destChemin.bringToFront();
@@ -50,14 +68,52 @@ public class MainActivity extends AppCompatActivity {
 
         costMessage.bringToFront();
         costValue.bringToFront();
+        stayShaked = true;
 
+
+            Drawable imgBack = img.getBackground();
+            imgBack.setVisible(true,true);
+
+
+        //rippleBack = (RippleBackground)img.getBackground();
+
+        //rippleBack.setActivated(true);
+        img.bringToFront();
         //Translation animation
 
-        Animation animation = new TranslateAnimation(0, 100,0, 0);
-        animation.setDuration(1000);
-        animation.setFillAfter(true);
-        costValue.startAnimation(animation);
-        costValue.setVisibility(View.INVISIBLE);
+        //Animation animation = new TranslateAnimation(0, 100,0, 0);
+        //animation.setDuration(1000);
+        //animation.setFillAfter(true);
+        //costValue.startAnimation(animation);
+        //costValue.setVisibility(View.INVISIBLE);
+
+
+// Create an object of the Android_Gesture_Detector  Class
+        Android_Gesture_Detector  android_gesture_detector  =  new Android_Gesture_Detector();
+// Create a GestureDetector
+        mGestureDetector = new GestureDetector(this, android_gesture_detector);
+
+
+               onShakeView(img);
+
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ObjectAnimator transAnimation= ObjectAnimator.ofFloat(img, "x", img.getPivotX(), img.getPivotX()+100);
+                //transAnimation.setDuration(3000);//set duration
+                //transAnimation.start();//start animation
+
+                //Animation animation = new TranslateAnimation(img.getPivotX(), img.getPivotX()+100,0, 0);
+                //animation.setDuration(1000);
+                //animation.setFillAfter(false);
+                //img.startAnimation(animation);
+                //img.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+
 
 
 
@@ -66,4 +122,22 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    public void onShakeView(View v) {
+        Animation shake;
+        int shaketrue;
+        shake = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
+
+        shake.setRepeatCount(Animation.INFINITE);
+
+            v.startAnimation(shake); // starts animation
+
+
+
+
+    }
+
+
+
 }
